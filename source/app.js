3f333366cfe543e2a3ffa97da972013b07635961
leehwongxing <leehwongxing@yandex.ru>
2017-10-20 01:39:04 +0700

    55555

--- source/app.js
+++ source/app.js
@@ -13,47 +13,6 @@ class app {
 
         this.port = 5000;
 
-        this.context.get('/webhook', function(req, res) {
-
-            if (req.query['hub.mode'] === 'subscribe' &&
-                req.query['hub.verify_token'] === process.env.VERIFYTOKEN) {
-
-                res.status(200).send(req.query['hub.challenge']);
-            } else {
-
-                console.error("Failed validation. Token mismatched");
-                res.sendStatus(403);
-            }
-        });
-
-        this.context.post('/webhook', function (req, res) {
-
-            var data = req.body;
-
-            console.log(data);
-            if(data.object === 'page') {
-
-                for (let request of data.entry) {
-
-                    var pageId = request.id;
-                    var eventTime = request.time;
-                    for (let event of request.messaging) {
-
-                        if (event.message) {
-                            this.proceedEvents(event);
-                        } else {
-                            console.log("Message not found in event: ", event);
-                        }
-                    }
-                }
-            }
-            res.sendStatus(200);
-        });
-
-        this.context.get('*', function (req, res){
-            res.status(200).send({"message":"it's running but wrong way dude!"});
-        });
-
         console.log("Environment port is %d", process.env.PORT);
     };
 
@@ -116,6 +75,48 @@ class app {
     };
 
     start() {
+
+        this.context.get('/webhook', function(req, res) {
+
+            if (req.query['hub.mode'] === 'subscribe' &&
+                req.query['hub.verify_token'] === process.env.VERIFYTOKEN) {
+
+                res.status(200).send(req.query['hub.challenge']);
+            } else {
+
+                console.error("Failed validation. Token mismatched");
+                res.sendStatus(403);
+            }
+        });
+
+        this.context.post('/webhook', function (req, res) {
+
+            var data = req.body;
+
+            console.log(data);
+            if(data.object === 'page') {
+
+                for (let request of data.entry) {
+
+                    var pageId = request.id;
+                    var eventTime = request.time;
+                    for (let event of request.messaging) {
+
+                        if (event.message) {
+                            this.proceedEvents(event);
+                        } else {
+                            console.log("Message not found in event: ", event);
+                        }
+                    }
+                }
+            }
+            res.sendStatus(200);
+        });
+
+        this.context.get('*', function (req, res){
+            res.status(200).send({"message":"it's running but wrong way dude!"});
+        });
+
         this.context.listen((process.env.PORT || this.port), '0.0.0.0');
     };
 }
